a806b15d710c9ff8053ba62a8b47a5e0a4c707e4
Adam Peacock <adam@thepeacock.net>
2022-06-22 09:23:53 -0400

    WIP before old code removal

--- json_mapper/mapper.py
+++ json_mapper/mapper.py
@@ -2,6 +2,7 @@
 
 from dataclasses import dataclass
 from typing import IO, Dict, List, Set, Tuple, Iterable, Union
+from functools import cached_property
 
 import json_stream
 from json_stream.base import TransientStreamingJSONObject, TransientStreamingJSONList
@@ -17,6 +18,132 @@ class Position:
     end_col: int
 
 
+class JSONMapper:
+    def __init__(self, io: IO):
+        if not io.seekable():
+            raise TypeError("Input IO must be seekable")
+
+        self._io = io
+
+    @cached_property
+    def all_positions(self) -> Dict[Tuple, Position]:
+        out: Dict[Tuple, Position] = {}
+
+        for key, start, end in self._get_key_positions_ranges:
+            start_line, start_col = self._get_line_col_for_position(start)
+            end_line, end_col = self._get_line_col_for_position(end)
+
+            out[key] = Position(
+                start_position=start,
+                start_line=start_line,
+                start_col=start_col,
+                end_position=end,
+                end_line=end_line,
+                end_col=end_col,
+            )
+
+        return out
+
+    @cached_property
+    def _line_break_positions(self) -> List[int]:
+        self._reset_io()
+
+        out: List[int] = []
+        line = self._io.readline()
+        while line:
+            out.append(self._io.tell() - 1)
+            line = self._io.readline()
+        return out
+
+    @cached_property
+    def _get_key_positions_ranges(self) -> List[Tuple[Tuple, int, int]]:
+        """Get every tuple key in the file, along with its start and end"""
+
+        self._reset_io()
+
+        root = json_stream.load(self._io)
+        # Where we are in the JSON file. Keys can be none (for the root),
+        # strings for objects, or ints for arrays
+        current_path: List[Union[None, str, int]] = [None]
+
+        def recurse(node) -> Iterable[Tuple[Tuple, int, int]]:
+            # Note that file positions are 1 based
+            started_at = self._io.tell()
+
+            # Depending on the object type, we might need to move
+            # the start or end positions. These are to make that easier.
+            start_offset = -1
+            end_offset = 0
+
+            if isinstance(node, TransientStreamingJSONObject):
+                for key, elem in node.items():
+                    current_path.append(key)
+                    yield from recurse(elem)
+
+            elif isinstance(node, TransientStreamingJSONList):
+                for i, elem in enumerate(node):
+                    current_path.append(i)
+                    yield from recurse(elem)
+
+            elif isinstance(node, str):
+                start_offset -= len(node)
+
+            elif isinstance(node, int):
+                end_offset -= 1
+                start_offset -= len(str(node))
+
+            elif isinstance(node, float):
+                end_offset -= 1
+                start_offset -= len(str(node))
+
+            elif isinstance(node, bool):
+                end_offset -= 1
+                start_offset -= len(str(node))
+
+            else:
+                # I don't think JSON has any kinds aside from those
+                # defined above, so this shouldn't ever be hit
+                raise TypeError(type(node))
+            ended_at = self._io.tell()
+
+            key = tuple(current_path[1:])
+            yield key, started_at + start_offset, ended_at + end_offset
+            current_path.pop()
+
+        return list(recurse(root))
+
+    def _get_line_col_for_position(self, position: int) -> Tuple[int, int]:
+        pass
+
+    def _get_line_for_position(self, position: int) -> int:
+        """Get just the line for a given position"""
+
+        line_breaks = self._line_break_positions
+
+        # We need to use price is right minus one rules - find the
+        # highest number line break that is *less than* the given position
+
+        # TODO: This is the naive approach that runs in O(n) time.
+        # It can be substituted with an approach that treats the
+        # list of line break positions as a binary tree and keeps
+        # bifurcating the list to run in O(log n) time
+
+        line_no = len(line_breaks)
+        for break_position in reversed(line_breaks):
+            # There are a bunch of off-by-one edge cases around here.
+            # They all cancel out.
+
+            if break_position < position:
+                return line_no
+
+            line_no -= 1
+
+        return line_no
+
+    def _reset_io(self):
+        self._io.seek(0)
+
+
 def key_map(io: IO) -> Dict[Tuple, Position]:
     """High level function interface to get all the key positions for an input"""
 
@@ -28,6 +155,7 @@ def key_map(io: IO) -> Dict[Tuple, Position]:
         needed_offsets.add(start)
         needed_offsets.add(end)
 
+    line_offsets = line_break_positions(io)
     offset_to_positions = get_offset_positions(needed_offsets)
     out: Dict[Tuple, Position] = {}
 
@@ -46,71 +174,13 @@ def key_map(io: IO) -> Dict[Tuple, Position]:
     return out
 
 
-def map_to_file_positions(io: IO) -> Iterable[Tuple[Tuple, int, int]]:
-    """Low level interface to get JSON paths to file offsets.
-    io input should already be at the position to start reading from"""
-
-    io.seek(0)
-
-    root = json_stream.load(io)
-    # Where we are in the JSON file. Keys can be none (for the root),
-    # strings for objects, or ints for arrays
-    current_path: List[Union[None, str, int]] = [None]
-
-    def recurse(node) -> Iterable[Tuple[Tuple, int, int]]:
-        # Note that file positions are 1 based
-        started_at = io.tell()
-
-        # Depending on the object type, we might need to move
-        # the start or end positions. These are to make that easier.
-        start_offset = 0
-        end_offset = 0
-
-        if isinstance(node, TransientStreamingJSONObject):
-            for key, elem in node.items():
-                current_path.append(key)
-                yield from recurse(elem)
-
-        elif isinstance(node, TransientStreamingJSONList):
-            for i, elem in enumerate(node):
-                current_path.append(i)
-                yield from recurse(elem)
-
-        elif isinstance(node, str):
-            start_offset -= len(node)
-
-        elif isinstance(node, int):
-            end_offset -= 1
-            start_offset -= len(str(node))
-
-        elif isinstance(node, float):
-            end_offset -= 1
-            start_offset -= len(str(node))
-
-        elif isinstance(node, bool):
-            end_offset -= 1
-            start_offset -= len(str(node))
-
-        else:
-            # I don't think JSON has any kinds aside from those
-            # defined above, so this shouldn't ever be hit
-            raise TypeError(type(node))
-        ended_at = io.tell()
-
-        key = tuple(current_path[1:])
-        yield key, started_at + start_offset, ended_at + end_offset
-        current_path.pop()
-
-    yield from recurse(root)
-
-
 def get_offset_positions(offsets: Iterable[int]) -> Dict[int, Tuple[int, int]]:
     """Find the line/column (1 indexed) for every provided offset"""
 
     last_line_no = 0
     last_offset = 0
 
-    out: Dict[int, Tuple[int, int]] = {}
+    out: Dict[int, Tuple[int, int]] = {0: (0, 0)}
 
     for line_no, offset in enumerate(sorted(offsets)):
         if offset > last_offset:
--- tests/mapper_test.py
+++ tests/mapper_test.py
@@ -1,12 +1,32 @@
 from pathlib import Path
-from json_mapper.mapper import key_map
+from json_mapper.mapper import JSONMapper
+from io import StringIO
 
 CURRENT_DIR = Path(__file__).parent
 
 
+def _get_json_mapper(file_name: Path) -> JSONMapper:
+    with open(CURRENT_DIR / file_name) as f_in:
+        io = StringIO(f_in.read())
+
+    return JSONMapper(io)
+
+
 def test_mapper_keys():
-    with open(CURRENT_DIR / "sample_1.json") as f_in:
-        data = key_map(f_in)
+    mapper = _get_json_mapper("sample_1.json")
 
     expected_keys = {tuple(), ("food",), ("bird",)}
-    assert data.keys() == expected_keys
+    actual_keys = mapper.all_positions.keys()
+    assert actual_keys == expected_keys
+
+
+def test_root_offset():
+    mapper = _get_json_mapper("sample_1.json")
+
+    root = mapper.all_positions[tuple()]
+    assert root.start_position == 0
+    assert root.end_position == 44
+    assert root.start_line == 0
+    assert root.start_col == 0
+    assert root.end_line == 3
+    assert root.end_col == 1
